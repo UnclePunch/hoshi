0f6378d66c68e268cccc1e33849a545f036e5dc9
UnclePunch <vinbertinelli@gmail.com>
2025-12-30 21:27:43 -0500

    screen_cam: fix function return type

--- include/hoshi/screen_cam.h
+++ include/hoshi/screen_cam.h
@@ -1,8 +1,10 @@
 #ifndef HOSHISCREENCAM_H
 #define HOSHISCREENCAM_H
 
+#include "text.h"
+
 #define HOSHI_SCREENCAM_GXLINK 63
 
-int Hoshi_CreateScreenText();
+Text *Hoshi_CreateScreenText();
 
 #endif
